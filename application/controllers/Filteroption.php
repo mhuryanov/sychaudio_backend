b605b53ca641c2c7dfa188e6140c2e6f4ecdcb02
Maxim <mhuryanov@gmail.com>
2019-07-17 10:11:32 -0700

    filter added

--- application/controllers/Filteroption.php
+++ application/controllers/Filteroption.php
@@ -6,10 +6,15 @@ require APPPATH . '/libraries/REST_Controller.php';
 
 class Filteroption extends REST_Controller
 {
+    private $postData;
+    private $header;
+
     public function __construct()
     {
         parent::__construct();       
         $this->load->model('filteropt_model');
+        $this->postData = $this->request->body;
+        $this->headers = $this->input->request_headers();
     }
 
     public function getallfilteroptions_get() {
@@ -20,6 +25,97 @@ class Filteroption extends REST_Controller
         $data['key'] = $this->filteropt_model->getFilterOptionsByWhere(array(), $this->filteropt_model->table_key);
 
         $this->set_response($data, REST_Controller::HTTP_OK);
+    }
+
+    public function filteroption_delete($type, $id)
+    {
+        if(!AUTHORIZATION::checkAdminAuth()) {
+            $this->set_response("Unauthorised", REST_Controller::HTTP_UNAUTHORIZED);
+        } else {
+            $id;
+            switch($type) {
+                case 'mood':
+                    $where['mood_id'] = $id;
+                    $this->filteropt_model->delete($where, 'tbl_mood');
+                    break;
+                case 'genre':
+                    $where['genre_id'] = $id;
+                    $this->filteropt_model->delete($where, 'tbl_genre');
+                    break;
+                case 'pace':
+                    $where['pace_id'] = $id;
+                    $this->filteropt_model->delete($where, 'tbl_pace');
+                    break;
+                case 'inst':
+                    $where['inst_id'] = $id;
+                    $this->filteropt_model->delete($where, 'tbl_instruments');
+                    break;
+            }
+
+            $this->set_response($id, REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function filteroption_post($type)
+    {
+        if(!AUTHORIZATION::checkAdminAuth()) {
+            $this->set_response("Unauthorised", REST_Controller::HTTP_UNAUTHORIZED);
+        } else {
+            $id;
+            switch($type) {
+                case 'mood':
+                    $data['mood_title'] = $this->postData['filter_title'];
+                    $id = $this->filteropt_model->add($data, 'tbl_mood');
+                    break;
+                case 'genre':
+                    $data['genre_title'] = $this->postData['filter_title'];
+                    $id = $this->filteropt_model->add($data, 'tbl_genre');
+                    break;
+                case 'pace':
+                    $data['pace_title'] = $this->postData['filter_title'];
+                    $id = $this->filteropt_model->add($data, 'tbl_pace');
+                    break;
+                case 'inst':
+                    $data['inst_title'] = $this->postData['filter_title'];
+                    $id = $this->filteropt_model->add($data, 'tbl_instruments');
+                    break;
+            }
+
+            $this->set_response($id, REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function filteroption_patch($type, $id)
+    {
+        if(!AUTHORIZATION::checkAdminAuth()) {
+            $this->set_response("Unauthorised", REST_Controller::HTTP_UNAUTHORIZED);
+        } else {
+            $id;
+            switch($type) {
+                case 'mood':
+                    $data['mood_title'] = $this->postData['filter_title'];
+                    $where['mood_id'] = $id;
+                    $this->filteropt_model->update($data, $where, 'tbl_mood');
+                    break;
+                case 'genre':
+                    $data['genre_title'] = $this->postData['filter_title'];
+                    $where['genre_id'] = $id;
+                    $this->filteropt_model->update($data, $where, 'tbl_genre');
+                    break;
+                case 'pace':
+                    $data['pace_title'] = $this->postData['filter_title'];
+                    $where['pace_id'] = $id;
+                    $this->filteropt_model->update($data, $where, 'tbl_pace');
+                    break;
+                case 'inst':
+                    $data['inst_title'] = $this->postData['filter_title'];
+                    $where['inst_id'] = $id;
+                    $this->filteropt_model->update($data, $where, 'tbl_instruments');
+                    break;
+            }
+
+            $this->set_response($id, REST_Controller::HTTP_OK);
+        }
     }
 
 }
--- application/models/filteropt_model.php
+++ application/models/filteropt_model.php
@@ -22,4 +22,28 @@ class Filteropt_model extends CI_Model
 		$result = $query->result_array();
 		return $result;
     }
+
+    public function add($data, $table_name) {
+        $this->db->trans_start();
+        $this->db->insert($table_name, $data);
+        
+        $insert_id = $this->db->insert_id();
+        
+        $this->db->trans_complete();
+        
+        return $insert_id;
+    }
+
+    public function update($data, $where, $table_name)
+    {
+        $this->db->where($where);
+        $this->db->update($table_name, $data);
+        return TRUE;
+    }
+
+    public function delete($where, $table_name)
+    {
+        $this->db->where($where);
+        $this->db->delete($table_name);
+    }
 }
